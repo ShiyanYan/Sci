030b3619735b9cb1d93254c65460974e81523926
ShiyanYan <yanshiyan.pku@gmail.com>
2014-08-13 15:00:25 -0400

    modified:   Simulation/Simu_PS_S.py

--- Simulation/Simu_PS_S.py
+++ Simulation/Simu_PS_S.py
@@ -15,6 +15,8 @@ exemplar = pickle.load(open(path + "exemplar.dump","r"))
 PaperNum = pickle.load(open(path + "AuthorPaperNum.dump","r"))
 Cohe = pickle.load(open(path + "SimInsideCoauthor.dump","r"))
 CluSimDic = pickle.load(open(path + "CluSimDic","r"))
+ClosestArea = pickle.load(open(path + "ClosestArea.dump","r"))
+
 def newAreaGenerate(AreaAlready):
     tt = len(exemplar)
     while True:
@@ -22,6 +24,25 @@ def newAreaGenerate(AreaAlready):
         newex = exemplar[newin]
         if not newex in AreaAlready:
             return newex
+
+
+def closestAreaGenerate(AreaAlready):
+    HstSim = 0
+    ClsArea = ""
+    for tot in AreaAlready:
+        if not tot in CluSimDic: continue
+        a1 = dict(ClosestArea[tot])
+        for nt in a1:
+            if nt in AreaAlready: continue
+            if ClosestArea[tot][nt]>HstSim:
+                HstSim = ClosestArea[tot][nt]
+                ClsArea = nt
+    if ClsArea!="": 
+        return ClsArea
+    else:
+        return newAreaGenerate(AreaAlready) #changed
+
+
 def MetriCal(Topics,alpha,beta,gamma):
     j = 0
     scorelist = {}
@@ -49,8 +70,8 @@ S1S = 0
 S2S = 0
 S3S = 0
 
-output = open(path + "SimulationPSResults_S.csv","wb")
-output.write("Alpha,Beta,Gamma,Simu1,Simu2,Simu3\n")
+output = open(path + "Simulation_PS_Results_S.csv","wb")
+output.write("Alpha,Beta,Gamma,Simu1,Simu2,Simu3,Simu4\n")
 for alpha in range(-5,6):
     for beta in range(-5,6):
         for gamma in range(-5,6):
@@ -62,24 +83,27 @@ for alpha in range(-5,6):
             S1S = 0
             S2S = 0 
             S3S = 0
+            S4S = 0
             for au in Authorlist:
                 if not au in AuMatchIDmatchTopics: continue
                 if not au in PaperNum: continue
                 cc += 1
                 if cc % 1000==0: print str(cc) + " Complete!"
                 Topics = {}
-                IdmatchTopics = AuMatchIDmatchTopics[au]
+                IdmatchTopics = dict(AuMatchIDmatchTopics[au])
                 if len(IdmatchTopics)<1: continue
+                ACP = 0
                 for Id in IdmatchTopics:
+                    if len(IdmatchTopics[Id])>0: ACP += 1
                     for tot in IdmatchTopics[Id]:
                          if tot in Topics: Topics[tot] += IdmatchTopics[Id][tot]
                          else: Topics[tot] = IdmatchTopics[Id][tot]
                 for tot in Topics:
-                    Topics[tot] = float(Topics[tot]) / float(len(IdmatchTopics))
+                    Topics[tot] = float(Topics[tot]) / float(ACP)
                 if len(Topics)<1: continue
                 scores0 = MetriCal(Topics,alpha,beta,gamma)
     
-                Topics2 = Topics
+                Topics2 = dict(Topics)
                 j = 0
                 for tot in sorted(Topics2,key=Topics2.get,reverse=True):
                     j += 1
@@ -89,28 +113,36 @@ for alpha in range(-5,6):
                         Topics2[tot] = Topics2[tot] * float(PaperNum[au]) / float(PaperNum[au] + 1) + float(1) / float(PaperNum[au] + 1)
                 scores1 = MetriCal(Topics2,alpha,beta,gamma)
     
-                Topics3 = Topics
+                Topics3 = dict(Topics)
                 for tot in Topics3:
                     Topics3[tot] = Topics3[tot] * float(PaperNum[au]) / float(PaperNum[au] + 1)
-                T = newAreaGenerate(Topics)
+                T = newAreaGenerate(Topics3)
                 Topics3[T] = float(1) / float(PaperNum[au] + 1)
-    
                 scores2 = MetriCal(Topics3,alpha,beta,gamma)
     
-                Topics4 = Topics3
+                Topics4 = dict(Topics3)
                 for tot in Topics4:
                     Topics4[tot] = Topics4[tot] * float(PaperNum[au] + 1) / float(PaperNum[au] + 2)
-                T = newAreaGenerate(Topics3)
+                T = newAreaGenerate(Topics4)
                 Topics4[T] = float(1) / float(PaperNum[au] + 2)
                 scores3 = MetriCal(Topics4,alpha,beta,gamma)
+                
+                Topics5 = dict(Topics)
+                for tot in Topics5:
+                    Topics5[tot] = Topics5[tot] * float(PaperNum[au]) / float(PaperNum[au] + 1)
+                T = closestAreaGenerate[Topics5]
+                Topics5[T] = float(1) / float(PaperNum[au] + 1)
+                scores4 = MetriCal(Topics5,alpha,beta,gamma)
+
                 totN += 1
                 if scores1<scores0: S1S += 1
                 if scores2>scores0: S2S += 1
                 if (scores2>scores0) and (scores3>scores2) and (scores3-scores2<scores2-scores0): S3S += 1
-
+                if scores4>scores0: S4S += 1
+            
 
             print "The Results" + " is:"
-            print "Simu1: " + str(float(S1S) / float(totN)) + " Simu2: " + str(float(S2S)/float(totN)) + " Simu3: " + str(float(S3S)/float(S2S))
-            output.write(str(alpha) + "," + str(beta) + "," + str(gamma) + "," + str(float(S1S)/float(totN)) + "," + str(float(S2S)/float(totN)) + "," + str(float(S3S)/float(S2S)) + "\n")
+            print "Simu1: " + str(float(S1S) / float(totN)) + " Simu2: " + str(float(S2S)/float(totN)) + " Simu3: " + str(float(S3S)/float(S2S)) + " Simu4: " + str(float(S4S)/float(totN))
+            output.write(str(alpha) + "," + str(beta) + "," + str(gamma) + "," + str(float(S1S)/float(totN)) + "," + str(float(S2S)/float(totN)) + "," + str(float(S3S)/float(S2S)) + "," + str(float(S4S)/float(totN)) + "\n")
 
 output.close()
